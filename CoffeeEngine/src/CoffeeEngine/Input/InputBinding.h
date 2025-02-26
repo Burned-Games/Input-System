7cb8272d41e9f85674c7cf67834c857a12734e85
Cifu2004 <albertitocibu@gmail.com>
2025-02-26 16:41:21 +0100

    InputBinding Comment

--- CoffeeEngine/src/CoffeeEngine/Input/InputBinding.h
+++ CoffeeEngine/src/CoffeeEngine/Input/InputBinding.h
@@ -20,6 +20,11 @@ namespace Coffee
         AxisCode Axis = Axis::Invalid;
         bool IsAnalog = false;
 
+        /**
+         * @brief Retrieves an input value based on whether it's analog or digital.
+         *
+         * @param controller The input source.
+         */
         float GetValue(ControllerCode controller);
 
     };
